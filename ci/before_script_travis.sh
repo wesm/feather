b5424eb17833eb9e9d01c4a1fb55d820ee06afca
Wes McKinney <wes@cloudera.com>
2016-02-06 20:21:13 -0800

    Add start of a Travis CI setup

---
+++ ci/before_script_travis.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Build an isolated thirdparty
+cp -r $TRAVIS_BUILD_DIR/thirdparty .
+./thirdparty/download_thirdparty.sh
+source thirdparty/versions.sh
+
+if [ $TRAVIS_OS_NAME == "osx" ]; then
+  # Only build gtest
+  ./thirdparty/build_thirdparty.sh gtest
+fi
+
+if [ $TRAVIS_OS_NAME == "linux" ]; then
+  ./thirdparty/build_thirdparty.sh
+  # Use a C++11 compiler on Linux
+  export CC="gcc-4.9"
+  export CXX="g++-4.9"
+fi
+
+export GTEST_HOME=$HOME/build_dir/thirdparty/$GTEST_BASEDIR
