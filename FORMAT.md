d3a19aa7d93c1f59436108368dde976b48c27892
Wes McKinney <wes@cloudera.com>
2016-02-07 21:09:51 -0800

    Add some format documentation, writer API prototyping

---
+++ FORMAT.md
@@ -0,0 +1,65 @@
+## Feather file format
+
+Here is the general structure of the file:
+
+```
+<4-byte magic number "FEA1">
+<ARRAY 0>
+<ARRAY 1>
+...
+<ARRAY n>
+<METADATA>
+<uint32: metadata size>
+<4-byte magic number "FEA1">
+```
+
+There is a stream of arrays laid out end-to-end, with the metadata appended on
+completion at the end of the file.
+
+For the arrays themselves, the memory layout is type dependent.
+
+1. Primitive arrays
+2. Variable-length (`BINARY` and `UTF8`) arrays
+3. Encoded variants of 1 and 2 (e.g. dictionary encoding)
+
+## Primitive arrays
+
+```
+<null bitmask, optional> <values>
+```
+
+The null bitmask is byte-aligned, but contains 1 bit per value indicating null
+or not null. For example, an array with length 5 containing 3 nulls followed by
+2 valid values would have a single byte with the values
+
+```
+1 1 1 0 0 0 0 0
+```
+
+The values is a contiguous array with elements equal to the fixed-width byte
+size.
+
+If, in the metadata, `null_count = 0`, then the null bitmask is omitted.
+
+## Variable-length arrays
+
+We use the Arrow encoding for storing variable-length values
+
+```
+<null bitmask, optional> <value offsets> <value data>
+```
+
+The null bitmask
+
+## Dictionary encoding
+
+Dictionary-encoded data is stored in the following layout.
+
+```
+<uint32: dictionary size>
+<array: dictionary values>
+<array: dictionary indices, int32 type>
+```
+
+The `total_bytes` stored in the metadata is the cumulative size of all of these
+pieces of data.
---
+++ src/feather/writer.h
@@ -0,0 +1,91 @@
+// Copyright 2016 Feather Developers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FEATHER_WRITER_H
+#define FEATHER_WRITER_H
+
+#include "feather/io.h"
+#include "feather/metadata.h"
+#include "feather/types.h"
+
+namespace feather {
+
+struct PrimitiveData {
+  PrimitiveType::type type;
+  int64_t length;
+  int64_t null_count;
+
+  // If null_count == 0, treated as nullptr
+  const uint8* nulls;
+
+  const void* data;
+
+  // For UTF8 and BINARY, not used otherwise
+  const int32_t* offsets;
+};
+
+struct CategoryData {
+  PrimitiveData indices;
+  PrimitiveData levels;
+  bool ordered;
+}
+
+struct DictionaryData {
+  PrimitiveData dict_values;
+  PrimitiveData indices;
+};
+
+class FileWriter;
+
+class TableWriter {
+ public:
+  void AppendPrimitive(const std::string& name, const PrimitiveData& values);
+  void AppendDictEncoded(const std::string& name, const DictionaryData& data);
+
+  void AppendCategory(const std::string& name, const PrimitiveData& values,
+      const PrimitiveData& levels, bool ordered = false);
+
+  void AppendTimestamp(const std::string& name, const PrimitiveData& values
+      const TimestampMetadata& meta);
+
+ private:
+  friend class FileWriter;
+
+  FileWriter* parent_;
+};
+
+class FileWriter {
+ public:
+  FileWriter(std::unique_ptr<OutputStream> stream);
+
+  // Append a table to the metadata
+  std::unique_ptr<TableWriter> AddTable(const std::string& name,
+      int64_t num_rows);
+
+  // We are done, write the file metadata and footer
+  void Finalize();
+
+ private:
+  std::unique_ptr<OutputStream> stream_;
+  metadata::FileBuilder metadata_;
+
+  // Append a primitive array to the file
+  void AppendPrimitiveArray();
+
+  void AppendVariableArray();
+};
+
+} // namespace feather
+
+#endif // FEATHER_WRITER_H
