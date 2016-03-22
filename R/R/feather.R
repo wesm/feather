b56cc81b46ac1d7d056cdc01167b84008e9bd5cb
hadley <h.wickham@gmail.com>
2016-03-22 10:17:39 -0500

    Expose R api

--- R/R/feather.R
+++ R/R/feather.R
@@ -1,3 +1,24 @@
 #' @useDynLib feather
 #' @importFrom Rcpp sourceCpp
 NULL
+
+#' Read and write feather files.
+#'
+#' @param path Path to feather file
+#' @name feather
+#' @return Both functions return a tibble/data frame. \code{write_feather}
+#'   invisibly returns \code{x} (so you can use this function in a pipeline).
+NULL
+
+#' @rdname feather
+#' @export
+read_feather <- function(path) {
+  readFeather(path)
+}
+
+#' @rdname feather
+#' @export
+write_feather <- function(x, path) {
+  writeFeather(x, path)
+  invisible(x)
+}
