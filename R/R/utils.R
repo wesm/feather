3fcdcb0138c9504245c1ca9518d7ef2c8fe87736
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-03-30 10:20:42 +0200

    [R]: new feather_examples()
    lists all example data sets as named vector

--- R/R/utils.R
+++ R/R/utils.R
@@ -1,10 +1,26 @@
 #' Get path to feather example files
 #'
+#' @description
+#' \code{feather_example} gets the path of a specific example
+#'
+#' @param x example file
 #' @export
 #' @keywords internal
 feather_example <- function(x) {
   system.file("feather", x, mustWork = TRUE, package = "feather")
 }
+
+#' @description
+#' \code{feather_examples} gets the path of all examples
+#'
+#' @export
+#' @rdname feather_example
+feather_examples <- function() {
+  path <- feather_example("")
+  stats::setNames(dir(path, full.names = TRUE), dir(path))
+}
+
+
 
 dim_desc <- function(x) {
   paste0("[", paste0(big_mark(x), collapse = " x "), "]")
