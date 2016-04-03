5a7769a921b3c738d10f7bd0bafda4066e277cff
Wes McKinney <wesm@apache.org>
2016-04-03 15:24:10 -0700

    [Python] Bump version to 0.2.0dev

--- python/setup.py
+++ python/setup.py
@@ -33,10 +33,10 @@ if Cython.__version__ < '0.19.1':
     raise Exception('Please upgrade to Cython 0.19.1 or newer')
 
 MAJOR = 0
-MINOR = 1
+MINOR = 2
 MICRO = 0
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-ISRELEASED = True
+ISRELEASED = False
 
 setup_dir = os.path.abspath(os.path.dirname(__file__))
 
