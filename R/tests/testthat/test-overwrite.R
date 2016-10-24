06ff3f30b8edc74858d4ffa2752880e5038dd757
Thomas Guillet <guillet.thomas@gmail.com>
2016-10-24 23:17:25 +0200

    [C++] Fix issue when overwriting feather files

---
+++ R/tests/testthat/test-overwrite.R
@@ -0,0 +1,15 @@
+context("overwrite")
+
+test_that("can read new data", {
+  path <- tempfile()
+
+  x <- 1:100
+  write_feather(dplyr::data_frame(x = x), path)
+  on.exit(file.remove(path))
+  res <- read_feather(path)
+
+  y <- x[1:(length(x)/2)]
+  write_feather(dplyr::data_frame(y = y), path)
+  resy <- read_feather(path)
+  expect_identical(resy$y, y)
+})
