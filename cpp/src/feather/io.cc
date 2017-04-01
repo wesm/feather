eb44c65d8f3b52d5c1d4aca95b05921e17a59c3e
Sergey Kirpichenko <c.sergey.kirpichenko@uptake.com>
2017-04-01 09:34:47 -0500

    c++ code formatting

--- cpp/src/feather/io.cc
+++ cpp/src/feather/io.cc
@@ -289,8 +289,8 @@ static inline Status FileWrite(int fd, const uint8_t* buffer, int64_t nbytes) {
   }
   ret = _write(fd, buffer, static_cast<unsigned int>(nbytes));
 #else
-  // (skirpichenko): even on x64 systems `write` does not allow to write more than 2GB at once
-  // so lets split data into blocks and write sequentially
+  // (skirpichenko): even on x64 systems `write` does not allow to write more than
+  // 2GB at once, so lets split data into blocks and write sequentially
   int64_t offset = 0;
   do {
     ret = write(fd, buffer + offset, std::min(nbytes - offset, int64_t(1 << 30)));
